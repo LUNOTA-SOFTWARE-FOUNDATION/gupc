88e16338ebce894f2cb3b5a46fb1a9b577a2c33a
Ian Moffett <ian@osmora.org>
2026-01-28 15:11:25 -0500

    gup: Add program types
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ inc/gup/types.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2026, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#ifndef GUP_TYPES_H
+#define GUP_TYPES_H 1
+
+#include "gup/token.h"
+
+/*
+ * Valid program data types
+ *
+ * @GUP_TYPE_BAD:   Invalid type
+ * @GUP_TYPE_VOID:  Type absent
+ */
+typedef enum {
+    GUP_TYPE_BAD,
+    GUP_TYPE_VOID
+} gup_type_t;
+
+/*
+ * Convert a token type into a program type
+ *
+ * @tt: Token type to convert
+ *
+ * Returns GUP_TYPE_BAD on failure
+ */
+static inline gup_type_t
+tok_to_type(tt_t tt)
+{
+    switch (tt) {
+    case TT_VOID:   return GUP_TYPE_VOID;
+    default:        return GUP_TYPE_BAD;
+    }
+
+    return GUP_TYPE_BAD;
+}
+
+#endif  /* !GUP_TYPES_H */
