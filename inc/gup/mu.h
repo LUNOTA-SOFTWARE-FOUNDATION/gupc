dda527f45517b03eaf4aa8eea220b1e92f2ad874
Ian Moffett <ian@osmora.org>
2026-01-28 19:07:46 -0500

    gup: Add initial codegen backend sources
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ inc/gup/mu.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#ifndef GUP_MU_H
+#define GUP_MU_H 1
+
+#include <stdbool.h>
+#include "gup/state.h"
+
+/*
+ * Emit a label into assembly
+ *
+ * @state:  Compiler state
+ * @label:  Label to emit
+ * @global: If true, label is global
+ *
+ * Returns zero on success
+ */
+int mu_emit_label(struct gup_state *state, const char *label, bool global);
+
+#endif  /* !GUP_MU_H */
---
+++ src/arch/x86_64.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026, Ian Moffett.
+ * Provided under the BSD-3 clause.
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include "gup/mu.h"
+
+int
+mu_emit_label(struct gup_state *state, const char *label, bool global)
+{
+    if (state == NULL || label == NULL) {
+        errno = -EINVAL;
+        return -1;
+    }
+
+    if (global) {
+        fprintf(
+            state->out_fp,
+            "[global %s]\n",
+            label
+        );
+    }
+
+    fprintf(
+        state->out_fp,
+        "%s:\n",
+        label
+    );
+
+    return 0;
+}
